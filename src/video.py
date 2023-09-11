e189e24add6640c4ee2ec05cc3c52b27f8808015
Mirudan <chaos.73@yandex.ru>
2023-09-11 21:40:09 +0400

    CHANGE: Class Video

--- src/video.py
+++ src/video.py
@@ -6,17 +6,17 @@ class Video(MixinBase):
 
     def __init__(self, video_id: str):
         """Экземпляр инициализируется по id видео. Дальше все данные будут подтягиваться по API."""
-        self.__video_id = video_id
-        self.video_response = (
-            self.get_service().videos().list(id=self.__video_id, part='snippet,statistics')).execute()
-
         try:
+            self.__video_id = video_id
+            self.video_response = (
+                self.get_service().videos().list(id=self.__video_id, part='snippet,statistics')).execute()
             self.title: str = self.video_response['items'][0]['snippet']['title']
             self.url: str = f"https://youtu.be/{self.__video_id}"
             self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
             self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
 
         except:
+            self.__video_id = video_id
             self.title: str = None
             self.url: str = None
             self.view_count: int = None
